612a014db9ef295326e6acd96994de15f403555c
Ruan Beukes <ruan.coder@gmail.com>
2016-03-26 14:29:08 +0800

    Create Console app for visual testing

--- DiamondKataConsole/Program.cs
+++ DiamondKataConsole/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DiamondKataLib;
+using System;
 
 namespace DiamondKataConsole
 {
@@ -10,6 +7,31 @@ namespace DiamondKataConsole
     {
         static void Main(string[] args)
         {
+            string input = "";
+            while (input != "EXIT")
+            {
+                Console.WriteLine("Print Diamond Pattern for: ");
+                input = Console.ReadLine().ToUpper();
+                if (input.Length >= 1)
+                {
+                    var chr = input.Substring(0, 1).ToCharArray()[0];
+                    if(chr >= 'A' && chr <= 'Z')
+                    {
+                        //print
+                        var array = new DiamondEngine(chr).CreateArray();
+                        for (int i = 0; i < array.GetLength(0); i++)
+                        {
+                            for (int k = 0; k < array.GetLength(1); k++)
+                            {
+                                var printVal = array[i, k].ToString();
+                                Console.Write(string.IsNullOrEmpty(printVal) ? "" : printVal);
+                            }
+
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
         }
     }
 }
